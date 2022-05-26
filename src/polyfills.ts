33a49eb43e4abe868da19e5042f61c41f25a85e7
Marc Queiroz <marc.queiroz@gmail.com>
2022-05-26 17:58:37 -0300

    Changing directory to polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -3,7 +3,7 @@
 import { NodePolyfillsOptions } from '.'
 
 const EMPTY_PATH = require.resolve(
-    'rollup-plugin-node-polyfills/polyfills/empty.js',
+    '@doiim/rollup-plugin-node-polyfills/polyfills/empty.js',
 )
 
 export function builtinsPolyfills() {
@@ -11,114 +11,114 @@ export function builtinsPolyfills() {
 
     libs.set(
         'process',
-        require.resolve('rollup-plugin-node-polyfills/polyfills/process-es6'),
+        require.resolve('@doiim/rollup-plugin-node-polyfills/polyfills/process-es6'),
     )
     libs.set(
         'buffer',
-        require.resolve('rollup-plugin-node-polyfills/polyfills/buffer-es6'),
+        require.resolve('@doiim/rollup-plugin-node-polyfills/polyfills/buffer-es6'),
     )
     libs.set(
         'util',
-        require.resolve('rollup-plugin-node-polyfills/polyfills/util'),
+        require.resolve('@doiim/rollup-plugin-node-polyfills/polyfills/util'),
     )
     libs.set('sys', libs.get('util'))
     libs.set(
         'events',
-        require.resolve('rollup-plugin-node-polyfills/polyfills/events'),
+        require.resolve('@doiim/rollup-plugin-node-polyfills/polyfills/events'),
     )
     libs.set(
         'stream',
-        require.resolve('rollup-plugin-node-polyfills/polyfills/stream'),
+        require.resolve('@doiim/rollup-plugin-node-polyfills/polyfills/stream'),
     )
     libs.set(
         'path',
-        require.resolve('rollup-plugin-node-polyfills/polyfills/path'),
+        require.resolve('@doiim/rollup-plugin-node-polyfills/polyfills/path'),
     )
     libs.set(
         'querystring',
-        require.resolve('rollup-plugin-node-polyfills/polyfills/qs'),
+        require.resolve('@doiim/rollup-plugin-node-polyfills/polyfills/qs'),
     )
     libs.set(
         'punycode',
-        require.resolve('rollup-plugin-node-polyfills/polyfills/punycode'),
+        require.resolve('@doiim/rollup-plugin-node-polyfills/polyfills/punycode'),
     )
     libs.set(
         'url',
-        require.resolve('rollup-plugin-node-polyfills/polyfills/url'),
+        require.resolve('@doiim/rollup-plugin-node-polyfills/polyfills/url'),
     )
     libs.set(
         'string_decoder',
         require.resolve(
-            'rollup-plugin-node-polyfills/polyfills/string-decoder',
+            '@doiim/rollup-plugin-node-polyfills/polyfills/string-decoder',
         ),
     )
     libs.set(
         'http',
-        require.resolve('rollup-plugin-node-polyfills/polyfills/http'),
+        require.resolve('@doiim/rollup-plugin-node-polyfills/polyfills/http'),
     )
     libs.set(
         'https',
-        require.resolve('rollup-plugin-node-polyfills/polyfills/http'),
+        require.resolve('@doiim/rollup-plugin-node-polyfills/polyfills/http'),
     )
-    libs.set('os', require.resolve('rollup-plugin-node-polyfills/polyfills/os'))
+    libs.set('os', require.resolve('@doiim/rollup-plugin-node-polyfills/polyfills/os'))
     libs.set(
         'assert',
-        require.resolve('rollup-plugin-node-polyfills/polyfills/assert'),
+        require.resolve('@doiim/rollup-plugin-node-polyfills/polyfills/assert'),
     )
     libs.set(
         'constants',
-        require.resolve('rollup-plugin-node-polyfills/polyfills/constants'),
+        require.resolve('@doiim/rollup-plugin-node-polyfills/polyfills/constants'),
     )
     libs.set(
         '_stream_duplex',
         require.resolve(
-            'rollup-plugin-node-polyfills/polyfills/readable-stream/duplex',
+            '@doiim/rollup-plugin-node-polyfills/polyfills/readable-stream/duplex',
         ),
     )
     libs.set(
         '_stream_passthrough',
         require.resolve(
-            'rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough',
+            '@doiim/rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough',
         ),
     )
     libs.set(
         '_stream_readable',
         require.resolve(
-            'rollup-plugin-node-polyfills/polyfills/readable-stream/readable',
+            '@doiim/rollup-plugin-node-polyfills/polyfills/readable-stream/readable',
         ),
     )
     libs.set(
         '_stream_writable',
         require.resolve(
-            'rollup-plugin-node-polyfills/polyfills/readable-stream/writable',
+            '@doiim/rollup-plugin-node-polyfills/polyfills/readable-stream/writable',
         ),
     )
     libs.set(
         '_stream_transform',
         require.resolve(
-            'rollup-plugin-node-polyfills/polyfills/readable-stream/transform',
+            '@doiim/rollup-plugin-node-polyfills/polyfills/readable-stream/transform',
         ),
     )
     libs.set(
         'timers',
-        require.resolve('rollup-plugin-node-polyfills/polyfills/timers'),
+        require.resolve('@doiim/rollup-plugin-node-polyfills/polyfills/timers'),
     )
     libs.set(
         'console',
-        require.resolve('rollup-plugin-node-polyfills/polyfills/console'),
+        require.resolve('@doiim/rollup-plugin-node-polyfills/polyfills/console'),
     )
-    libs.set('vm', require.resolve('rollup-plugin-node-polyfills/polyfills/vm'))
+    libs.set('vm', require.resolve('@doiim/rollup-plugin-node-polyfills/polyfills/vm'))
     libs.set(
         'zlib',
-        require.resolve('rollup-plugin-node-polyfills/polyfills/zlib'),
+        require.resolve('@doiim/rollup-plugin-node-polyfills/polyfills/zlib'),
     )
     libs.set(
         'tty',
-        require.resolve('rollup-plugin-node-polyfills/polyfills/tty'),
+        require.resolve('@doiim/rollup-plugin-node-polyfills/polyfills/tty'),
     )
     libs.set(
         'domain',
-        require.resolve('rollup-plugin-node-polyfills/polyfills/domain'),
+        require.resolve('@doiim/rollup-plugin-node-polyfills/polyfills/domain'),
     )
 
     // not shimmed
@@ -136,14 +136,14 @@ export function builtinsPolyfills() {
 
     // libs.set(
     //     'fs',
-    //     require.resolve('rollup-plugin-node-polyfills/polyfills/browserify-fs'),
+    //     require.resolve('@doiim/rollup-plugin-node-polyfills/polyfills/browserify-fs'),
     // )
 
-    // TODO enable crypto and fs https://github.com/ionic-team/rollup-plugin-node-polyfills/issues/20
+    // TODO enable crypto and fs https://github.com/ionic-team/@doiim/rollup-plugin-node-polyfills/issues/20
     // libs.set(
     //     'crypto',
     //     require.resolve(
-    //         'rollup-plugin-node-polyfills/polyfills/crypto-browserify',
+    //         '@doiim/rollup-plugin-node-polyfills/polyfills/crypto-browserify',
     //     ),
     // )
 
